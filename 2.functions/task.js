f2f228f19b5a53e07ce9db09d173784017bd60bb
Natalya Smirnova <kyz.natali@mail.ru>
2025-03-05 13:50:31 +0300

    functions newest

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -72,6 +72,10 @@ function summElementsWorker(...arr) {
     
 
     function makeWork (arrOfArr, func) {
+      if (arrOfArr.length === 0) {
+        return 0; 
+    }
+
       let maxWorkerResult = -Infinity;
     
       for (let i = 0; i < arrOfArr.length; i++) {
@@ -79,14 +83,10 @@ function summElementsWorker(...arr) {
         if (result > maxWorkerResult) {
           maxWorkerResult = result;
         }
-        if (makeWork === 0) {
-          return 0; 
-      }
+       
       }
     
       return maxWorkerResult;
     }
-    const arr = [[0]];
-    makeWork(arr, averageEvenElementsWorker)
-
-
+     const arr = [[10, 10, 11, 20, 10], [67, 10, 2, 39, 88], [72, 75, 51, 87, 43], [30, 41, 55, 96, 62]];
+makeWork(arr, averageEvenElementsWorker)
