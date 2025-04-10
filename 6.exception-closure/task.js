ae4a578ebf5e76f287844b10cc03e37e6121ea2a
Natalya Smirnova <kyz.natali@mail.ru>
2025-04-10 19:43:34 +0300

    exception-closure add

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,62 @@
-﻿
+﻿function validateCount(value) {
+    function parseCount(value) {
+      const result = Number.parseFloat(value);
+      if (isNaN(result)) {
+        throw new Error("Невалидное значение");
+      }
+      return result;
+    }
+    
+    try {
+      return parseCount(value); 
+    } catch (error) {
+      return {
+        error: "Невалидное значение"
+      };
+    }
+  }
+
+
+  class Triangle {
+    constructor(side1, side2, side3) {
+      if (side1 + side2 < side3 || 
+          side1 + side3 < side2 || 
+          side2 + side3 < side1) {
+        throw new Error("Треугольник с такими сторонами не существует");
+      }
+      this.side1 = side1;
+      this.side2 = side2;
+      this.side3 = side3;
+    }
+  
+    get perimeter() {
+      return this.side1 + this.side2 + this.side3;
+    }
+  
+    get area() {
+      const p = this.perimeter / 2;
+      const area = Math.sqrt(p * (p - this.side1) * 
+                            (p - this.side2) * (p - this.side3));
+      return parseFloat(area.toFixed(3));
+    }
+  }
+  
+    function getTriangle(side1, side2, side3) {
+    try {
+      return new Triangle(side1, side2, side3);
+    } catch (error) {
+      return {
+        get perimeter() {
+          return "Ошибка! Треугольник не существует";
+        },
+        get area() {
+          return "Ошибка! Треугольник не существует";
+        }
+      };
+    }
+  }
+  
+const triangle = new Triangle(6,10,15);
+console.log(triangle);
+console.log(triangle.perimeter);
+console.log(triangle.area);
